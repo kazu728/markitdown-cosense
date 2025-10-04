7bce8256f1a87151db6bbc80eaa44e1173c46d6e
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2025-10-04 18:09:16 +0900

    Bump version to 0.2.0

--- src/markitdown_cosense/__init__.py
+++ src/markitdown_cosense/__init__.py
@@ -2,7 +2,7 @@
 
 from ._plugin import __plugin_interface_version__, register_converters
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "register_converters",
     "__plugin_interface_version__",
